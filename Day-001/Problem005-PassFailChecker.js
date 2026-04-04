cb2d1290fcb036563412ae317c84eebf9fd67c88
randomUser0147 <iyogesh.more1998@gmail.com>
2026-04-04 12:12:34 +0530

    feat(day-001): Problem 005: Added Pass or fail solution.

---
+++ Day-001/Problem005-PassFailChecker.js
@@ -0,0 +1,12 @@
+// Problem Statement 005: Determine if marks result in pass or fail (≥40 = pass).
+// Input Example: 35
+// Expected Output: Fail
+
+const marks = 35;
+
+if (marks >= 40) {
+      console.log("Pass");
+}
+else {
+      console.log("Fail");
+}
